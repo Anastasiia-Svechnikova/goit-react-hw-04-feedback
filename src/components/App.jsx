b35d4a5e2b38aef1a0d5c708b88ae2b0bb9befbe
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-16 17:12:40 +0300

    useReduce refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,54 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Section from './Section/section';
 import Statistics from './Statistics/statistics';
 import FeedbackOptions from './Feedback-options/feedback-options';
 import Notification from './Notification/notification';
 
+// const optionTags = ['good', 'bad', 'neutral']
+
+
+
 export const App = () => {
-  const [neutral, setNeutral] = useState(0);
-  const [good, setGood] = useState(0);
-  const [bad, setBad] = useState(0);
+// setState.......... *3
 
-  const onLeaveFeedback = opt => {
-    switch (opt) {
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
+  // const onLeaveFeedback = opt => {
+  //   switch (opt) {
+  //     case 'neutral':
+  //       setNeutral(prevState => prevState + 1);
+  //       break;
+  //     case 'good':
+  //       setGood(prevState => prevState + 1);
+  //       break;
+  //     case 'bad':
+  //       setBad(prevState => prevState + 1);
+  //       break;
 
-      default:
-        console.log(`invalid option ${opt}`);
-    }
-  };
+  //     default:
+  //       console.log(`invalid option ${opt}`);
+  //   }
+  // };
 
-  const optionTags = ['good', 'bad', 'neutral']
+  const options = {
+    neutral: 0,
+    good: 0,
+    bad: 0,
+  }
+  
+  const optionTags = Object.keys(options)
+
+  const reducer = (state, action) => {
+    
+    const { type, payload } = action;   
+    return { ...state, [type]: state[type] + payload}
+  }
+
+  const [{ good, neutral, bad }, dispatch] = useReducer(reducer, options);
+  // const [state, dispatch] = useReducer(reducer, options);
+  
+  const onLeaveFeedback = (opt) => {
+  dispatch({type: opt, payload: 1})
+}
+
   const total = [good, bad, neutral].reduce((acc, el) => (acc += el), 0);
   const positivePercentage = (good * 100) / total;
 
